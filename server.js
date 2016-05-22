b8e6f71d990d4e3a29878c3c8c3eb91f8ba9628d
JOSH KNEELAND <josh@joshdoesdesign.com>
2016-05-22 15:31:55 -0600

    Changed res.send to index.html

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('index');
+	res.send('index.html');
 });
 
 var port = Number(process.env.PORT || 3000);
