a9bf311165b5c9ef804ed6a7a90cf7da03c033fa
JOSH KNEELAND <josh@joshdoesdesign.com>
2016-05-22 15:28:12 -0600

    Added original angular files to the new node server project

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('<h1>Hello World</h1>');
+	res.end('index');
 });
 
 var port = Number(process.env.PORT || 3000);
