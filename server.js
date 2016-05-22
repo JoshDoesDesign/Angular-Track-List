4e6697b2f7734ba57ae368b46c7c761c41f005b9
JOSH KNEELAND <josh@joshdoesdesign.com>
2016-05-22 15:40:37 -0600

    Added express to the app

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-var http = require('http');
+var express = require('express')
+var app = express()
 
-var server = http.createServer(function(req, res) {
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+/*var server = http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.send('index.html');
 });
 
 var port = Number(process.env.PORT || 3000);
 
-server.listen(port);
+server.listen(port);*/
